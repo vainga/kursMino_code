3769abdd87b00b5b608bdf9329160dcb7db21745
vain <vain.dlaigor@mail.ru>
2023-11-08 23:23:13 +0700

    class implementations

--- KursCode/Program.cs
+++ KursCode/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace KursCode
+﻿namespace KursCode
 {
 
     class Program
--- KursCode/workerClass.cs
+++ KursCode/workerClass.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
-namespace Clients
+﻿namespace Clients
 {
     class workerClass: clientsClass
     {
