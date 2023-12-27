969493bd6327b014a4621561df329815155094fb
vain <vain.dlaigor@mail.ru>
2023-12-27 10:18:13 +0700

    UC создаются

--- KursCode/MVVM/View/UserControls/ClientsUserControlMini.xaml.cs
+++ KursCode/MVVM/View/UserControls/ClientsUserControlMini.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Clients;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System;
+using System.Drawing.Imaging;
+
 
 namespace KursCode.MVVM.View.UserControls
 {
@@ -24,6 +30,19 @@ namespace KursCode.MVVM.View.UserControls
         public ClientsUserControlMini()
         {
             InitializeComponent();
+        }
+
+        private Image ConverImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+
+            Image image;
+            using (MemoryStream ms = new MemoryStream(imageBytes))
+            {
+                Image image = Image.FromStream(ms);
+            }
+
+            return image;
         }
 
         public void SetData(workerClass data)
